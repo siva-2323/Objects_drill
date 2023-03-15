ce254477ce6e9918f1f2aed02940a73ffb546adb
Siva <sivar15racer@gmail.com>
2023-03-15 16:44:55 +0530

    modified defaultProps

--- defaults.cjs
+++ defaults.cjs
@@ -3,14 +3,14 @@ function isObject(input)
 {
 if( input === null || input===undefined ) return false;
 else if(typeof input === "object") return true;
-else return false;
+return false;
 }
 
 function defaults(input,defaultProps)
 {
-    if( defaultProps === null || defaultProps === undefined || typeof defaultProps !== "object" || Array.isArray(defaultProps)) return input;
     if(isObject(input) && !Array.isArray(input))
         {
+            if( defaultProps === null || defaultProps === undefined || typeof defaultProps !== "object" || Array.isArray(defaultProps)) return input;
             for(let temp in defaultProps){
                 if(!temp in input){
                     input[temp]=defaultProps[temp];
@@ -20,7 +20,7 @@ function defaults(input,defaultProps)
             }
             return input;
 
-        }else return {};
+        }return {};
 }
 
 module.exports=defaults;
--- test/testDefaults.cjs
+++ test/testDefaults.cjs
@@ -3,5 +3,5 @@ let batmanCasting={cast1:"unknown", cast2:"Christian Bale", cast3:"Ben "};
 let defaults=require("../defaults.cjs");
 
 
-let temp=defaults(testObject,testObject);
+let temp=defaults(testObject,batmanCasting);
 console.log(temp);
