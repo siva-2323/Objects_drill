1598f51242f2465d1abae8f15d78e3882eb19a80
Siva <sivar15racer@gmail.com>
2023-03-16 00:49:34 +0530

    simplified and tested every possible input values

--- pairs.cjs
+++ pairs.cjs
@@ -1,32 +1,15 @@
 
 let answer=[];
 
-let keys=require("./keys.cjs");
-let values=require("./values.cjs");
-
-
-function isObject(input)
-{
-if((input === null)||(input===undefined)) return false;
-else if(typeof input === "object") return true;
-return false;
-}
-
 function pairs(input)
 {
-    if(isObject(input) && !Array.isArray(input))
+       
+    for(let key in input)
     {
+        answer.push([key,input[key]])
 
-        let keyArray=keys(input);
-        let valueArray=values(input);
-        let arrayLength=keyArray.length;
-
-        for(let loop=0;loop<arrayLength;loop++)
-        {
-            answer.push([keyArray[loop],valueArray[loop]]);
-        }
+    }
             return answer;
-    }return answer;
 
 }
 
--- test/testPairs.cjs
+++ test/testPairs.cjs
@@ -1,9 +1,19 @@
 const pairs=require("../pairs.cjs");
 
 
-const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; 
+let func=() => {};
+let nullValue=null;
+let undefinedValue=undefined;
+let emptyString="";
+let emptyArray=[];
+let emptyObject={};
+let number=5;
+let floatNumber=6.9;
+let testObject={ name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+let string="Shiva";
+let booleanValue=true;
+let symbol="$";
+let items=["lets","check","this"];
 
-
-let temp=pairs(testObject);
-
-console.log(temp);
+  console.log(pairs(testObject));
+    
