3ae7a81aaf30615ee4fe1a675090c5751c197e98
Siva <sivar15racer@gmail.com>
2023-03-15 23:15:37 +0530

    simplified and tested every possible input value

--- keys.cjs
+++ keys.cjs
@@ -1,23 +1,13 @@
+
 let answer=[];
 
-function isObject(input)
-{
-if( input === null || input===undefined ) return false;
-else if(typeof input === "object") return true;
-return false;
-}
-
-
 function keys(input)
-{
-        if(isObject(input) && !Array.isArray(input))
-        {       
+{    
                 for(let value in input)
                 {
                     answer.push(value);
                 }
-                
-        }
+
         return answer;
 
 }
--- test/testKeys.cjs
+++ test/testKeys.cjs
@@ -1,7 +1,22 @@
 const keys=require("../keys.cjs");
 
-const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+
+let func=() => {};
+let nullValue=null;
+let undefinedValue=undefined;
+let emptyString="";
+let emptyArray=[];
+let emptyObject={};
+let number=5;
+let floatNumber=6.9;
+let testObject={ name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+let string="Shiva";
+let booleanValue=true;
+let symbol="$";
+let items=["lets","check","this"];
+
+    console.log(keys(symbol));
+    
 
 
-let temp=keys(testObject);
-console.log(temp);
+
