c821b74baf2f722ce77086837368717bf32a8d79
Siva <sivar15racer@gmail.com>
2023-03-16 14:06:02 +0530

    Checked defaultProps input value

--- defaults.cjs
+++ defaults.cjs
@@ -1,26 +1,22 @@
 
-function isObject(input)
-{
-if( input === null || input===undefined ) return false;
-else if(typeof input === "object") return true;
-return false;
-}
+
 
 function defaults(input,defaultProps)
 {
-    if(isObject(input) && !Array.isArray(input))
-        {
-            if( defaultProps === null || defaultProps === undefined || typeof defaultProps !== "object" || Array.isArray(defaultProps)) return input;
-            for(let temp in defaultProps){
-                if(!temp in input){
-                    input[temp]=defaultProps[temp];
-                }else{
-                    input[temp]=defaultProps[temp]
-                }
+    
+    if(typeof input !== "object" || Array.isArray(input))
+    {
+         return {};
+    }else
+    {
+            for (let keys in defaultProps) 
+            {
+                
+                input[keys] = defaultProps[keys]
             }
-            return input;
-
-        }return {};
+   
+    }
+    return input;
 }
 
 module.exports=defaults;
--- test/testDefaults.cjs
+++ test/testDefaults.cjs
@@ -2,19 +2,9 @@
 let batmanCasting={cast1:"unknown", cast2:"Christian Bale", cast3:"Ben "};
 let defaults=require("../defaults.cjs");
 
-let func=() => {};
-let nullValue=null;
-let undefinedValue=undefined;
-let emptyString="";
-let emptyArray=[];
-let emptyObject={};
-let number=5;
-let floatNumber=6.9;
-let testObject={ name: 'Bruce Wayne', age: 36, location: 'Gotham' };
-let string="Shiva";
-let booleanValue=true;
-let symbol="$";
-let items=["lets","check","this"];
 
-  console.log(defaults(testObject,batmanCasting));
+let testObject="Shiva"
+
+
+  console.log(defaults( batmanCasting,testObject));
     
